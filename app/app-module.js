d3ab182562ea4ce9b7b91b0820ae5770b62a74b8
Eric Waring <ericw@the-feel.co.uk>
2016-01-07 13:09:11 +0000

    Add articles module

--- app/app-module.js
+++ app/app-module.js
@@ -10,6 +10,7 @@
     .module('mwCom', [
       'ui.router',
       'mgcrea.ngStrap',
-      'home'
+      'home',
+      'articles'
     ]);
 }());
--- build.config.js
+++ build.config.js
@@ -4,7 +4,7 @@ var outDir = 'build/';
 
 module.exports = {
   host: 'localhost',
-  port: 3000,
+  port: 3002,
 
   // app directories
   appDir: 'app',
